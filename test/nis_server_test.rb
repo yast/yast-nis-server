cd3301d9ba1c0c70ca99644ecf182e64dd072dfd
Josef Reidinger <jreidinger@suse.cz>
2020-03-25 18:54:02 +0100

    replace routines old testsuite

---
+++ test/nis_server_test.rb
@@ -0,0 +1,61 @@
+require_relative "test_helper"
+
+Yast.import "NisServer"
+
+describe "Yast::NisServer" do
+  subject { Yast::NisServer }
+  describe "#getMaster" do
+    it "returns nil if domain is empty" do
+      subject.domain = ""
+      expect(subject.getMaster).to eq nil
+    end
+
+    it "returns nil if domain directory does not exists" do
+      subject.domain = "test"
+      allow(Yast::SCR).to receive(:Read).with(path(".target.size"), "/var/yp/test").and_return(-1)
+
+      expect(subject.getMaster).to eq nil
+    end
+
+    it "returns nil if domain directory is empty" do
+      subject.domain = "test"
+      allow(Yast::SCR).to receive(:Read).with(path(".target.size"), "/var/yp/test").and_return(0)
+      allow(Yast::SCR).to receive(:Read).with(path(".target.dir"), "/var/yp/test").and_return([])
+
+      expect(subject.getMaster).to eq nil
+    end
+
+    it "return master server name" do
+      subject.domain = "test"
+      allow(Yast::SCR).to receive(:Read).with(path(".target.size"), "/var/yp/test").and_return(0)
+      allow(Yast::SCR).to receive(:Read).with(path(".target.dir"), "/var/yp/test").and_return(["a"])
+      allow(Yast::SCR).to receive(:Execute)
+        .with(
+          path(".target.bash_output"),
+          "/usr/lib/yp/makedbm -u /var/yp/test/a | grep ^YP_MASTER_NAME")
+        .and_return("exit" => 0, "stdout" => "YP_MASTER_NAME expected")
+
+      expect(subject.getMaster).to eq "expected"
+    end
+  end
+
+  describe "isYPMaster" do
+    it "returns false if master is nil" do
+      allow(subject).to receive(:getMaster).and_return(nil)
+
+      expect(subject.isYPMaster).to eq false
+    end
+
+    it "returns true if master is same as hostname" do
+      allow(subject).to receive(:getMaster).and_return("test")
+      allow(Yast::SCR).to receive(:Execute)
+        .with(
+          path(".target.bash_output"),
+          "/usr/lib/yp/yphelper --hostname")
+        .and_return("exit" => 0, "stdout" => "test")
+
+      expect(subject.isYPMaster).to eq true
+    end
+
+  end
+end
